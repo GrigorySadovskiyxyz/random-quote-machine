217442178593163f7b2c21baaf3bdb69abab92cb
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-10-05 16:02:14 +0300

    Working ver

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Button from 'react-bootstrap/Button';
+
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class FetchRandomQuote extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            author: '',
+            content: '',
+            link: 'https://twitter.com/intent/tweet'
+        }
+        this.fetchQuotes = this.fetchQuotes.bind(this);
+      }
+
+      componentDidMount() {
+          this.fetchQuotes();
+      }
+
+      tweetLink = () => {
+        let tweetMe = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + this.state.content + '"' + ' - ' + this.state.author + ' #quotes')
+        this.setState({link: tweetMe});
+      }
+
+      fetchQuotes = () => {
+        fetch('https://api.quotable.io/random')
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ author: data.author });
+            this.setState({ content: data.content });
+        })
+      }
+
+      render() {
+        return (
+            <div className="wrapper" id="quote-box"> 
+            <p>Random Quote Machine</p>
+            <a href={this.state.link} id="tweet-quote" onClick={this.tweetLink}>Tweet quote</a>
+            <Button id="new-quote" onClick={this.fetchQuotes} variant="outline-light">New quote</Button>{' '}
+            <h1 id="text"><q>{this.state.content}</q></h1>
+            <p id="author">- {this.state.author}</p>
+        </div>
+        );
+}
 }
 
-export default App;
+export default FetchRandomQuote;
